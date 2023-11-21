a73e9aa9e321c32ae3996c080da8b1207ef596ea
Stefania <stefania.struzzi@gmail.com>
2023-11-21 16:26:11 +0100

    creata ApiRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const server= express()
 
 const port= 3001
 
+server.use("/api", apiRouter);
+
 // creo endpoint TEST
 
-server.get("/test", (req, res)=>{
-    res.json({message:"Hello, world!"})
-})
+// server.get("/test", (req, res)=>{
+//     res.json({message:"Hello, world! "})
+// })
 
 server.listen(port,()=>{
     console.log("Server is listening on port....: " + port )
